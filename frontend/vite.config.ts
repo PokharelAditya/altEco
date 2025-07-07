dbdcdaa39940ab62326d1c16e8700cfd877801cf
Aditya Pokharel <adityapokharel63@gmail.com>
2025-07-07 19:31:10 +0545

    Frontend Access through Network IP Address

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-
-server: {
+  server: {
+    host: true,
     proxy: {
-      '/api':'http://localhost:4000'
-
-    }
+      "/api": "http://localhost:4000",
+    },
   },
   plugins: [react(), tailwindcss()],
-})
+});
