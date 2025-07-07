532fcc57973b3ba0fdb38280686fcfab506e87d0
Aditya Pokharel <adityapokharel63@gmail.com>
2025-07-08 04:05:31 +0545

    Barcode UI

--- frontend/src/components/subcomponents/BarcodeScanner.tsx
+++ frontend/src/components/subcomponents/BarcodeScanner.tsx
@@ -1,61 +1,72 @@
-import { useEffect, useRef, useState } from "react"
-import { BrowserMultiFormatReader, BrowserCodeReader } from "@zxing/browser"
-// import { BrowserCodeReader } from "@zxing/browser"
-// import { DecodeHintType, BarcodeFormat } from "@zxing/library"
+import { useEffect, useRef, useState } from "react";
+import { BrowserMultiFormatReader } from "@zxing/browser";
 
 const BarcodeScanner: React.FC = () => {
-  
-  const [ code, setCode ] = useState<string>("")
-  const videoRef = useRef<HTMLVideoElement>(null) 
+  const [code, setCode] = useState<string>("");
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    const codeReader = new BrowserMultiFormatReader()
-    // new Map().set(
-    //   DecodeHintType.POSSIBLE_FORMATS, [
-    //   BarcodeFormat.QR_CODE,
-    //   BarcodeFormat.EAN_13,
-    //   BarcodeFormat.UPC_A,
-    //   BarcodeFormat.CODE_128,
-    //   BarcodeFormat.EAN_8,
-    //   BarcodeFormat.UPC_E,
-    //   BarcodeFormat.CODE_39,
-    //
-    //   ]
-    // ))    
-      
-      codeReader.decodeFromVideoDevice(
-      undefined,
-      videoRef.current!,
-      (result, _, controls) => {
-        if (result) {
-          setCode(result.getText())
-          console.log("Scanned code: " )
-          controls.stop()
-          // BrowserCodeReader.releaseAllStreams()
-          // BrowserCodeReader.cleanVideoSource(videoRef.current!)
-        }
-      }
-    )
+    if (!code) {
+      const codeReader = new BrowserMultiFormatReader();
 
-    // return () => {
-    //   controlsPromise.then((controls) => {
-    //     controls.stop()
-    //   })
-    // }
-  }, [code])
+      codeReader.decodeFromVideoDevice(
+        undefined,
+        videoRef.current!,
+        (result, _, controls) => {
+          if (result) {
+            setCode(result.getText());
+            controls.stop();
+          }
+        },
+      );
+    }
+  }, [code]);
 
   return (
     <>
-    <div className="max-w-screen max-h-screen overflow-hidden">
-     <video
-        ref={videoRef}
-        className="w-full h-full rounded-2xl"
-      />
-        <button onClick = {() => setCode("")}>Cancel</button>
-      <div className="absolute dark:text-white">{code}</div>
+      <div className="pt-8">
+        {code === "" ? (
+          <video
+            ref={videoRef}
+            className="w-full max-h-[70svh] rounded-2xl object-cover"
+          />
+        ) : (
+          <div className="flex-col">
+            <div className="flex-col text-center">
+              <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-2">
+                Scan Successful
+              </h1>
+              <p className="text-gray-600 dark:text-gray-400">Code: {code}</p>
+            </div>
+            <div className="flex mt-4 gap-4 w-full justify-center">
+              <button
+                className="flex items-center gap-2 mt-4 px-4 py-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600 rounded-md hover:shadow-md cursor-pointer transition-all duration-300"
+                onClick={() => setCode("")}
+              >
+                Cancel
+              </button>
+              <button className="flex items-center gap-2 mt-4 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 hover:shadow-md cursor-pointer transition-all duration-300">
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m21 21-4.34-4.34" />
+                  <circle cx="11" cy="11" r="8" />
+                </svg>
+                <span>Search</span>
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default BarcodeScanner;
--- frontend/src/components/subcomponents/PromptInput.tsx
+++ frontend/src/components/subcomponents/PromptInput.tsx
@@ -10,9 +10,18 @@ const PromptInput: React.FC = () => {
           placeholder="Enter product description..."
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
-          className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm placeholder-gray-400 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-colors" 
+          className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm placeholder-gray-400 bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-colors"
         />
-        <button className="ml-auto flex items-center gap-2 mt-4 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 hover:shadow-md cursor-pointer transition-all duration-300">
+        <div className="flex gap-4 w-full justify-end">
+        <button
+          className="flex items-center gap-2 mt-4 px-4 py-2 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-600 rounded-md hover:shadow-md cursor-pointer transition-all duration-300"
+          onClick={() => setPrompt("")}
+        >
+          Cancel
+        </button>
+        <button 
+            disabled={prompt===""}
+            className="flex disabled:bg-gray-700 items-center gap-2 mt-4 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 hover:shadow-md not-disabled:cursor-pointer transition-all duration-300">
           <svg
             width="20"
             height="20"
@@ -28,6 +37,7 @@ const PromptInput: React.FC = () => {
           </svg>
           <span>Search</span>
         </button>
+        </div>
       </div>
     </>
   );
