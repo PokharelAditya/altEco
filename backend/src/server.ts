00db6a08e80ff3ecd3816cb42200be1d015710f8
Aditya Pokharel <adityapokharel63@gmail.com>
2025-07-07 19:29:15 +0545

    Backend accessible for all hosts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,18 @@
-import app from './app';
-import env from './util/ValidateEnv';
-import pool from './database';
+import app from "./app";
+import env from "./util/ValidateEnv";
+import pool from "./database";
 
 const port = env.PORT || 6666;
 
-pool.connect()
+pool
+  .connect()
   .then(() => {
-    console.log('Connected to PostgreSQL');
-    app.listen(port, () => {
+    console.log("Connected to PostgreSQL");
+    app.listen(port, "0.0.0.0", () => {
       console.log(`Server running on port: ${port}`);
     });
   })
-  .catch((error:unknown) => {
-    console.error('Failed to connect to PostgreSQL:', error);
+  .catch((error: unknown) => {
+    console.error("Failed to connect to PostgreSQL:", error);
     process.exit(1);
   });
