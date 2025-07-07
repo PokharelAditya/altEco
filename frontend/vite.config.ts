d01ef61cbb778927c6e3723d7bd5952a5dbd8ac8
Aditya Pokharel <adityapokharel63@gmail.com>
2025-07-07 20:24:14 +0545

    https link generated using ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:4000",
     },
+    allowedHosts: [
+      // Changed this using ngrok for https 
+      // "2777-2400-1a00-b060-b9b4-417a-c191-9eab-57b5.ngrok-free.app",
+    ],
   },
   plugins: [react(), tailwindcss()],
 });
