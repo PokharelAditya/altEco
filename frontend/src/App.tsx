5043b2f37ea6cc09fa4408f7c80fa336819ea032
Aditya Pokharel <adityapokharel63@gmail.com>
2025-07-07 19:25:54 +0545

    For search products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Login from './components/Login'
-import { AuthContextProvider } from './context/AuthContext'
-import Home from './components/Home'
-import Logout from './components/Logout'
-import SignUp from './components/SignUp'
-import UserSignupData from "./components/UserSignupData"
-import UserPreference from "./components/UserPreference"
+import Login from "./components/Login";
+import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./components/Home";
+import Logout from "./components/Logout";
+import SignUp from "./components/SignUp";
+import UserSignupData from "./components/UserSignupData";
+import UserPreference from "./components/UserPreference";
+import SearchProduct from "./components/SearchProduct";
+
 function App() {
   return (
     <AuthContextProvider>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-        <Route path="/" element={<Home/>} />
-          <Route path ="/login" element={<Login/>}/>
-          <Route path = "/logout" element={<Logout/>}/>
-          <Route path = "/signup" element={<SignUp/>}/>
-          <Route path="/signup-detail" element={<UserSignupData/>}/>
-          <Route path="/set-preference" element={<UserPreference/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup-detail" element={<UserSignupData />} />
+          <Route path="/set-preference" element={<UserPreference />} />
+          <Route path="/search-product" element={<SearchProduct />} />
         </Routes>
       </Router>
     </AuthContextProvider>
---
+++ frontend/src/components/SearchProduct.tsx
@@ -0,0 +1,19 @@
+// import BarcodeScanner from "./subcomponents/BarcodeScanner";
+
+const SearchProduct: React.FC = () => {
+  return (
+    <>
+      <div className="flex">
+        <button className="">
+          Scan Barcode
+        </button>
+        <button className="">
+          Input Prompt
+        </button>
+      </div>
+      {/* <BarcodeScanner /> */}
+    </>
+  );
+};
+
+export default SearchProduct;
