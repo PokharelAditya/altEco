6304e6ca8b30cd09853de1b855ee7c6afb1ebf5d
adinteressant <rochak.adh98@gmail.com>
2025-07-15 08:06:52 +0545

    recommendation works

--- backend/src/db/products.ts
+++ backend/src/db/products.ts
@@ -1,7 +1,7 @@
 import pool from "./setupDB";
 
 
-export async function fetchProducts() {
+export async function fetchProducts(userId:string) {
     const { rows } = await pool.query(`SELECT * FROM product`)
     return rows;
 }
