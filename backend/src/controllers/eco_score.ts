7efa1e96f52ef568700aa4187ae17f820b95054b
adinteressant <rochak.adh98@gmail.com>
2025-07-15 08:46:41 +0545

    eco score calculator

--- backend/src/controllers/eco_score.ts
+++ backend/src/controllers/eco_score.ts
@@ -11,6 +11,11 @@ export const ecoscoreController = (req: Request, res: Response) => {
   const ecoScore = calculateEcoScore(tags);
    res.json({ ecoScore });
 }
+const rms = (tags:string[],TagWeights:Record<string,number>):number => {
+   let rms = tags.reduce((sum, tag) => sum + (TagWeights[tag]**2 || 0), 0)
+    rms = Math.sqrt(rms)
+    return rms
+}
 
 const calculateEcoScore = (tagString: string): number => {
   if (!tagString.trim()) return 50;
@@ -44,10 +49,13 @@ const calculateEcoScore = (tagString: string): number => {
     "sachet": 2
   };
 
-  const posScore = tags.reduce((sum, tag) => sum + (positiveTagWeights[tag] || 0), 0);
-  const negScore = tags.reduce((sum, tag) => sum + (negativeTagWeights[tag] || 0), 0);
-  const rawScore = 50 + (posScore - negScore) * 5;
+  let posScore = tags.reduce((sum, tag) => sum + (positiveTagWeights[tag] || 0), 0);
+  if(posScore!=0)posScore = posScore/rms(tags,positiveTagWeights)
+  let negScore = tags.reduce((sum, tag) => sum + (negativeTagWeights[tag] || 0), 0);
+  if(negScore!=0) negScore = negScore/rms(tags,negativeTagWeights)
+  const rawScore = 50 + (posScore - negScore) * 10;
 
-  return Math.max(0, Math.min(100, rawScore));
+  // return Math.max(0, Math.min(100, rawScore));
+  return rawScore>50 ? Math.ceil(rawScore) : Math.floor(rawScore)
 };
 
